a078e6d7d9292fe2841bf0acc5f9974c6a1af050
Alexandr Rusin <i@arusin.ru>
2025-08-29 13:05:51 +0700

    Тест iframe 3

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("[main page] js run");
 
   const iframe = document.querySelector("#my-iframe");
-  let iframeLoaded = false;
 
   const btnTest = document.querySelector(".btn-test");
   const btnClose = document.querySelector(".btn-close");
@@ -15,15 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
     sendMessage("test");
   });
 
-  iframe.addEventListener("load", function () {
-    iframeLoaded = true;
-  });
-
   function sendMessage(eventType) {
     const data = { eventType };
-
-    if (iframeLoaded) {
-      iframe.contentWindow.postMessage(JSON.stringify(data), iframe.src);
-    }
+    iframe.contentWindow.postMessage(JSON.stringify(data), iframe.src);
   }
 });
